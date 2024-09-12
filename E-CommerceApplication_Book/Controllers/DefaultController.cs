9fd27059b739bdddd381acebdff82246027dcedc
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-09-12 23:16:29 +0530

    a

--- E-CommerceApplication_Book/Controllers/DefaultController.cs
+++ E-CommerceApplication_Book/Controllers/DefaultController.cs
@@ -11,7 +11,7 @@ namespace E_CommerceApplication_Book.Controllers
         // GET: Default
         public ActionResult Index()
         {  
-            return View(); ;
+            return View(); ; ;
         }
     }
 }
